e61b9ee5f061fb9de48395681960a7f082e2d658
Ismael <fs.ismael@gmail.com>
2022-07-30 00:21:21 -0300

    fix: add proper exception encapsulation

--- src/main/java/com/github/ifsantos/pdf/io/IOHandler.java
+++ src/main/java/com/github/ifsantos/pdf/io/IOHandler.java
@@ -28,11 +28,11 @@ public class IOHandler {
 	private static Logger log = LoggerFactory.getLogger(IOHandler.class);
 
 	public byte[]  readImage(String imagePath) {
-		log.info("Reading image file "+imagePath);
+		log.info("Reading image file {}",imagePath);
 		try {
 			return Files.readAllBytes(Path.of(imagePath));
 		} catch (Exception e) {
-			throw new RuntimeException(e);
+			throw new IORuntimeException(e);
 		}
 	}
 	public byte[] getImageBytes(BufferedImage image) {
@@ -41,7 +41,7 @@ public class IOHandler {
 			ImageIO.write(image, "png", bos);
 			return bos.toByteArray();
 		} catch (IOException e) {
-			throw new RuntimeException("Error getting image bytes",e);
+			throw new IORuntimeException("Error getting image bytes",e);
 		}
 	}
 
@@ -50,7 +50,7 @@ public class IOHandler {
 		try {
 			return ImageIO.read(new ByteArrayInputStream(imageBytes));
 		} catch (IOException e) {
-			throw new RuntimeException(e);
+			throw new IORuntimeException(e);
 		}
 	}
 
@@ -60,7 +60,7 @@ public class IOHandler {
 			Image img = Image.getInstance(imageBytes);
 			document.add(img);
 		} catch (Exception e) {
-			throw new RuntimeException(e);
+			throw new IORuntimeException(e);
 		}
 	}
 
@@ -76,8 +76,16 @@ public class IOHandler {
 			Files.createDirectories(outputFilePath.getParent());
 			PdfWriter.getInstance(document, new FileOutputStream(outputFilePath.toFile()));
 		} catch (Exception e) {
-			throw new RuntimeException(e);
+			throw new IORuntimeException(e);
 		}
 		return document;
+	}
+	class IORuntimeException extends RuntimeException{
+		public IORuntimeException(String s, Throwable t){
+			super(s,t);
+		}
+		public IORuntimeException(Throwable t){
+			super(t);
+		}
 	}
 }
