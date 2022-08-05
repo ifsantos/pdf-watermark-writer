47935eeb687bd57abc232497a67e388b259a9a93
Ismael <fs.ismael@gmail.com>
2022-08-05 00:26:50 -0300

    feat: add test on try with parameters

---
+++ src/test/java/com/github/ifsantos/TryWithParametersTest.java
@@ -0,0 +1,38 @@
+package com.github.ifsantos;
+
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class TryWithParametersTest {
+    Logger log = LoggerFactory.getLogger(getClass());
+    @Test
+    void throwsFileNotFoundException() {
+        assertThrowsExactly(FileNotFoundException.class, () -> {
+            try(FileInputStream fis = new FileInputStream(".\new.txt")){
+                fis.read();
+            }
+        });
+    }
+
+    @Test
+    void throwsIOException(){
+        assertThrowsExactly(IOException.class, () -> {
+            try(InputStream is = getClass().getClassLoader().getResourceAsStream("test.txt")){
+                is.close();
+                is.read();
+            }
+        });
+    }
+
+}
